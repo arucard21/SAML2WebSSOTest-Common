72c3af478b32b22fc48a21e89009430e2aa53262
rmokiem-topdesk <r.mokiem@topdesk.com>
2014-08-15 11:04:13 +0200

    Separated the common classes that can be used for testing both SP and IdP.

---
+++ src/main/java/saml2tester/common/SAMLAttribute.java
@@ -0,0 +1,28 @@
+package saml2tester.common;
+/**
+ * The Attribute class contains the values pertaining to a single attribute
+ */
+public class SAMLAttribute {
+
+	private String attributeName;
+	private String nameFormat;
+	private String attributeValue;
+	
+	public SAMLAttribute(String name, String format, String value){
+		attributeName = name;
+		nameFormat = format;
+		attributeValue = value;
+	}
+	
+	public String getAttributeName() {
+		return attributeName;
+	}
+
+	public String getNameFormat() {
+		return nameFormat;
+	}
+
+	public String getAttributeValue() {
+		return attributeValue;
+	}
+}
